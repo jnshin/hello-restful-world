fe6666eb778c856a3d3a6e6e065238d75ed502b2
Jaenyun Shin <tlswosus@gmail.com>
2018-08-22 15:29:24 +0900

    Add basic HTTP service code

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello RESTful World!")
+
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %q",
+				html.EscapeString(r.URL.Path))
+		})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
